3d8195232ec0beb401e6d8fe0f50fa24acfa538e
kshirish <kisleyshirish25@gmail.com>
2018-02-20 08:39:04 +0530

    label routes added

---
+++ routes/label.js
@@ -0,0 +1,58 @@
+const Label = require("../models/label");
+const { pick } = require("lodash");
+
+module.exports = router => {
+  router.post("/label", (req, res, next) => {
+    const obj = pick(req.body, ["txt"]);
+
+    obj.createdBy = req.decoded._id;
+
+    const label = new Label(obj);
+
+    label
+      .save()
+      .then(label => res.json(label))
+      .catch(next);
+  });
+
+  router.delete("/label/:id", (req, res, next) => {
+    const labelId = req.params.id;
+
+    Label.remove({ _id: labelId })
+      .then(result => res.json(result))
+      .catch(next);
+  });
+
+  router.put("/label/:id", (req, res, next) => {
+    const labelId = req.params.id;
+    const options = { new: true };
+    const obj = pick(req.body, ["txt"]);
+
+    obj.updatedAt = Date.now();
+
+    Label.findByIdAndUpdate(labelId, obj, options)
+      .then(label => res.json(label))
+      .catch(next);
+  });
+
+  router.get("/label/:id", (req, res, next) => {
+    const labelId = req.params.id;
+
+    Label.findById(labelId)
+      .then(label => res.json(label))
+      .catch(next);
+  });
+
+  router.get("/label", (req, res, next) => {
+    const page = req.query.page ? +req.query.page : 1;
+    const limit = req.query.limit ? +req.query.limit : 10;
+    const sortOptions = req.query.sort ? { [req.query.sort]: 1 } : {};
+
+    Label.find({})
+      .sort(sortOptions)
+      .skip(limit * page - limit)
+      .limit(limit)
+      .then(labels => res.json(labels))
+      .catch(next);
+  });
+};
